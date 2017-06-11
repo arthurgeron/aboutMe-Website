6ade88f86864e5f4c3c493bb658078dd2a752b28
Arthur Ribeiro Geron <arthurgeron@hotmail.com>
2017-06-11 12:36:37 -0300

    Added images to timeline

--- js/custom-angular-timeline.js
+++ js/custom-angular-timeline.js
@@ -14,14 +14,14 @@ var ExperienceTimeline = function($rootScope, $document, $timeout, $scope) {
 		badgeClass: 'info',
 		badgeIconClass: 'glyphicon-briefcase',
 		title: 'Daitan Group',
-		titleContentHtml: '<h3>Full Stack Developer</h3>',
+		titleContentHtml: '<h3>Full Stack Developer</h3><img class="timeline-img" src="images/daitan.png"></img>',
 		when: 'May 2017 - Current',
 		contentHtml: '<p>Work with sip and HTTP, Java, Erlang, Jersey, Feign, RibbonMQ, Guice and other technologies to create freeswitch and web solutions for client companies that work with PABXs</p>'
 	}, {
 		badgeClass: 'info',
 		badgeIconClass: 'glyphicon-briefcase',
 		title: 'Empresa Junior Obelisco Soluções em Sofware',
-		titleContentHtml: '<h3>Chief Executive Officer </h3>',
+		titleContentHtml: '<h3>Chief Executive Officer </h3><img class="timeline-img" src="images/obelisco.png"></img>',
 		when: 'July 2016 - Current',
 		contentHtml: "<p>Represent Empresa Júnior Obelisco - Soluções em Software;</p><p>Develop, maintain and follow the company's Business Plan;</p><p>Summon council meetings;</p><p>Develop and plan joint actions with the Technical and Administrative Directors;</p><p>Issue normative instructions for the internal regulations</p><p>Raise demands and needs of the community and / or potential clients;</p><p>Issue Executive Orders for the dissolution of departments and / or creation of new departments;</p><p>Manage meetings with the Administrative Council for management and / or administrative decisions.</p>"
 	},  {
@@ -29,14 +29,14 @@ var ExperienceTimeline = function($rootScope, $document, $timeout, $scope) {
 		badgeIconClass: 'glyphicon-education',
 		title: 'Bearing Point - Trinity College Dublin ',
 		when: 'January 2016 – July 2016',	
-		titleContentHtml: '<h3>Full Stack Developer Intern</h3>',
+		titleContentHtml: '<h3>Full Stack Developer Intern</h3> <img class="timeline-img" src="images/tcd.png"></img>',
 		contentHtml: 'Project, develop and deploy a Web System and Database for BearingPoint on behalf of Trinity College Dublin. BearingPoint tested and approved the web-based software, which replaces the tools used in the past to manage and visualize the employee’s productiveness.',
 		footerContentHtml: ''
 	}, {
 		badgeClass: 'info',
 		badgeIconClass: 'glyphicon-wrench',
 		title: 'Dell Computers Inc.',
-		titleContentHtml: '<h3>Intern Test Engineer</h3>',
+		titleContentHtml: '<h3>Intern Test Engineer</h3><img class="timeline-img" src="images/dell.png"></img>',
 		contentHtml: "<p>My activities at Dell include:</p><p>-Web Systems deployment for the Electrical and Mechanical Repair(EMR) team;</p><p>-Tools deployment and support for Test Engineering Portal, EMR and ARB;</p><p>-Support EMR and Asset Recover Business(ARB) teams on failure analysis;</p><p>-Support PILOTs of new products and blocks updates;</p><p>-Create/manage work instructions of interactive functional tests;</p><p>-Support for the Quality team on First Pass Yield analysis;</p>",
 		when: 'August 2014 - August 2015',
 		footerContentHtml: ''
@@ -44,7 +44,7 @@ var ExperienceTimeline = function($rootScope, $document, $timeout, $scope) {
 		badgeClass: 'info',
 		badgeIconClass: 'glyphicon-education',
 		title: 'Federal Institute Of Sao Paulo',
-		titleContentHtml: '<h3>Teaching Assistant</h3>',
+		titleContentHtml: '<h3>Teaching Assistant</h3><img class="timeline-img" src="images/ifsp.png"></img>',
 		contentHtml: "Give lectures on the following subjects: Programming Algorithms, Programming Languages and Data Structure l.",
 		when: 'January 2014 – May 2015',
 		footerContentHtml: ''
@@ -52,7 +52,7 @@ var ExperienceTimeline = function($rootScope, $document, $timeout, $scope) {
 		badgeClass: 'info',
 		badgeIconClass: 'glyphicon-eye-open',
 		title: 'CTI Renato Archer',
-		titleContentHtml: '<h3>Volunteer Researcher</h3>',
+		titleContentHtml: '<h3>Volunteer Researcher</h3><img class="timeline-img" src="images/cti.png"></img>',
 		contentHtml: "Work as a Researcher at the Movements Analysis using Vicon equipment; Capture and process human movement data, which would then be transferred to a Animated 3D Doll using Blender Software.",
 		when: 'February 2014 – July 2014',
 		footerContentHtml: ''
